77cc4dd63da1cb87b0f3a3ce71d0cb70a8f402b0
Pablo Estrada <pablof300@gmail.com>
2018-12-19 16:22:02 -0500

    Added the message system

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,11 @@ class MessagesController < ApplicationController
   def create
     message = Message.new(message_params)
     if message.save
+      flash[:success] = 'Thank you for contacting me, I will be in touch shortly.'
+      redirect_to root_path
     else
+      flash.now[:error] = 'Some of your information in your message is incorrect'
+      render 'pages/index'
     end
   end
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def index
+    @message = Message.new
   end
 end
