976d8f91e3417a4b105b50c394171dc9c1120f06
Pablo Estrada <pablof300@gmail.com>
2019-12-27 13:53:34 -0500

    Small update to the UserDAOTest

--- service/src/test/java/me/pabloestrada/core/UserDAOTest.java
+++ service/src/test/java/me/pabloestrada/core/UserDAOTest.java
@@ -1,4 +1,26 @@
 package me.pabloestrada.core;
 
+import me.pabloestrada.core.user.User;
+import me.pabloestrada.core.user.UserDAO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class UserDAOTest {
+
+    private static UserDAO userDAO;
+
+    @BeforeAll
+    static void setUp(){
+        userDAO = new UserDAO();
+    }
+
+    @DisplayName("Testing adding a new user is successful")
+    @Test
+    void testAddingSingleUser() {
+        final User user = new User("Pablo", "123456");
+        userDAO.insertUser(user.getUsername(), user.getPassword());
+        Assertions.assertEquals(userDAO.getUser("Pablo").get(), user);
+    }
 }
