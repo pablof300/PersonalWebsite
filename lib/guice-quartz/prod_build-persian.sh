6243c4aa66b4f9edddf20468be9b9679192f288b
Pablo Estrada <pablof300@gmail.com>
2020-03-11 00:02:26 -0400

    Fixed relative paths in prod_build-persian.sh

--- lib/guice-quartz/prod_build-persian.sh
+++ lib/guice-quartz/prod_build-persian.sh
@@ -2,4 +2,4 @@
 
 echo "Compiling and uploading to local repository of Persian"
 mvn clean package -f lib/guice-quartz/
-mvn install:install-file -Dfile=lib/guice-quartz/target/guice-quartz-sample.jar -DpomFile=pom.xml -Dsources=lib/guice-quartz/target/guice-quartz-sample-sources.jar -DlocalRepositoryPath=service/local-persian-repository -f lib/guice-quartz/
+mvn install:install-file -Dfile=target/guice-quartz-sample.jar -DpomFile=pom.xml -Dsources=target/guice-quartz-sample-sources.jar -DlocalRepositoryPath=../../service/local-persian-repository -f lib/guice-quartz/
