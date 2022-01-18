6596ae77f46618ac0effeed245a2120a0c756806
Pablo Estrada <pablof300@gmail.com>
2022-01-18 15:34:56 -0500

    Removed ReactGA

--- src/screens/MainApp/App.tsx
+++ src/screens/MainApp/App.tsx
@@ -2,7 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MenuComponent from '../../components/Navbar';
 import LandingScreen from '../LandingScreen';
+// import createBrowserHistory from 'history'
+// import ReactGA from 'react-ga';
 import './App.css';
+
+// const history = createBrowserHistory()
+// ReactGA.initialize('UA-171727538-1');
+// ReactGA.pageview(window.location.pathname + window.location.search);
+// history.listen((location) => {
+//   console.log("####????????")
+//   ReactGA.pageview(location.pathname + location.search);
+//   console.log(location)
+// });
 
 const App = () => (
   <>
